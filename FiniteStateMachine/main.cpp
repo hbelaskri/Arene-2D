4c62967f192b998ff22eebc3447bbf5bdd21611f
hbelaskri <hbelaskri@gaming.tech>
2026-02-06 13:00:20 +0100

    commit
    supp background et map

--- FiniteStateMachine/main.cpp
+++ FiniteStateMachine/main.cpp
@@ -1,39 +1,37 @@
 #include <SFML/Graphics.hpp>
 #include "Player.h"
-#include "Background.h"
 
-int main() 
-{
-    Background background;
-    if (!background.load("../Assets/pacmanPack/Background.png"))
-    {
-        return -1; 
-    }
 
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "PacMan ");
-    window.setFramerateLimit(60);
-
-    Player player;
-
-    sf::Clock clock;
-
-    while (window.isOpen()) 
-    {
-        window.clear(sf::Color::Black);
-
-        background.draw(window); 
-        player.draw(window);     
-
-        float deltaTime = clock.restart().asSeconds();
-
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) window.close();
-        }
-
-        player.update(deltaTime);
-        player.draw(window);
-        window.display();
-    }
-
-    return 0;
-}
+//int main() 
+//{
+//   /* Background background;
+//    if (!background.load("../Assets/pacmanPack/Background.png"))
+//    {
+//        return -1; 
+//    }*/
+//
+//    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "PacMan ");
+//    window.setFramerateLimit(60);
+//
+//    Player player;
+//
+//    sf::Clock clock;
+//
+//    while (window.isOpen()) 
+//    {
+//        window.clear(sf::Color::Black);
+//        player.draw(window);     
+//
+//        float deltaTime = clock.restart().asSeconds();
+//
+//        while (const std::optional event = window.pollEvent()) {
+//            if (event->is<sf::Event::Closed>()) window.close();
+//        }
+//
+//        player.update(deltaTime);
+//        player.draw(window);
+//        window.display();
+//    }
+//
+//    return 0;
+//}
